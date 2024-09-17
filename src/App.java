6886490e5b20477e193810e4ef7b22880fec5e12
vadikkamskiy <vadikkamskiy@gmail.com>
2024-09-17 18:59:22 +0300

    create 4 task

--- src/App.java
+++ src/App.java
@@ -6,6 +6,18 @@ public class App {
        var cat = 3.6;
        var papper = 763789;
        System.out.println(dog + "\n" + cat + "\n" + papper);
+       dog += 4;
+       cat += 4;
+       papper += 4;
+       System.out.println("\n" + dog + "\n" + cat + "\n" + papper);
+       dog -= 3.5;
+       cat -= 1.6;
+       papper -= 7639;
+       System.out.println("\n" + dog + "\n" + cat + "\n" + papper);
+       var friend = 19;
+       System.out.println(friend);
+       System.out.println(friend += 2);
+       System.out.println(friend /= 7);
     }
     
 }
