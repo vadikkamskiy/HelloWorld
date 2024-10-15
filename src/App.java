acca9ec0d66fc8a56c612208ce1a0fdb6b0194d3
vadikkamskiy <vadikkamskiy@gmail.com>
2024-10-15 20:49:31 +0300

    homework3 task1

--- src/App.java
+++ src/App.java
@@ -1,15 +1,37 @@
+import java.text.MessageFormat;
 
 public class App {
     
     public static void main(String[] args) throws Exception {
-        String ft = firstTask();
-        System.out.println(ft);
+        firstTask();
+        secondTask();
     }
-    public static String firstTask(){
-        String output = "";
-        //find something about $variables
-        output += " ";
-        return output;
+    public static void firstTask(){
+        int integerVariable = Integer.MAX_VALUE;
+        String integerOutput = MessageFormat.format("Значение переменной integerVariable с типом int равно {0}", integerVariable);
+        System.out.println(integerOutput);
+
+        byte byteVariable = Byte.MAX_VALUE;
+        String byteOutput = MessageFormat.format("Значение переменной byteVariable с типом byte равно {0}", byteVariable);
+        System.out.println(byteOutput);
+
+        short shortVariable = Short.MAX_VALUE;
+        String shortOutput = MessageFormat.format("Значение переменной shortVariable с типом short равно {0}", shortVariable);
+        System.out.println(shortOutput);
+
+        Long longVariable = Long.MAX_VALUE;
+        String longOutput = MessageFormat.format("Значение переменной longVariable с типом long равно {0}", longVariable);
+        System.out.println(longOutput);
+
+        float floatVariable = Float.MAX_VALUE;
+        String floatOutput = MessageFormat.format("Значение переменной floatVariable с типом float равно {0}", floatVariable);
+        System.out.println(floatOutput);
+
+        Double doubleVariable = Double.MAX_VALUE;
+        String doubleOutput = MessageFormat.format("Значение переменной doubleVariable с типом double равно {0}", doubleVariable);
+        System.out.println(doubleOutput);
     }
-    
+    public static void secondTask(){
+        
+    }
 }
