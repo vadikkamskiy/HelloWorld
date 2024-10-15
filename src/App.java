e502d077a7cc0160c18679743e2acaaf7d3439c1
vadikkamskiy <vadikkamskiy@gmail.com>
2024-10-15 21:48:05 +0300

    Second task

--- src/App.java
+++ src/App.java
@@ -23,15 +23,34 @@ public class App {
         String longOutput = MessageFormat.format("Значение переменной longVariable с типом long равно {0}", longVariable);
         System.out.println(longOutput);
 
-        float floatVariable = Float.MAX_VALUE;
+        float floatVariable = 14.5456f;
         String floatOutput = MessageFormat.format("Значение переменной floatVariable с типом float равно {0}", floatVariable);
         System.out.println(floatOutput);
 
-        Double doubleVariable = Double.MAX_VALUE;
+        Double doubleVariable = 3453.4567568876;
         String doubleOutput = MessageFormat.format("Значение переменной doubleVariable с типом double равно {0}", doubleVariable);
         System.out.println(doubleOutput);
     }
     public static void secondTask(){
+        Object[] variables = new Object[7];
+        float varOne = 27.12f;
+        long varTwo = 987678965549L;
+        double varThree = 2.786;
+        short varFour = 569;
+        var varFive = -159;
+        int varSix = 27897;
+        byte varSeven = 67;
+
+        variables[0] = varOne;
+        variables[1] = varTwo;
+        variables[2] = varThree;
+        variables[3] = varFour;
+        variables[4] = varFive;
+        variables[5] = varSix;
+        variables[6] = varSeven;
         
+        for(int i = 0; i < 7; i++){
+            System.out.println(variables[i]);
+        }
     }
 }
